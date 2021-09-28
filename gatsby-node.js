2ae987dc6eb1467469ebce43edb43625bde59b3a
Taylor Dean <taylordean787@gmail.com>
2021-09-28 11:08:35 -0500

    added dynamic routing

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,33 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path")
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ page, actions, graphql }) => {
+  const { createPage } = actions
+
+  const { data } = await graphql(`
+    query Projects {
+      allSanityProject {
+        nodes {
+          name
+          id
+          date
+        }
+      }
+    }
+  `)
+
+  data.allSanityProject.nodes.forEach(node => {
+    actions.createPage({
+      path: `/projects/${node.id}`,
+      component: path.resolve("./src/pages/projects/project-details.js"),
+      context: { slug: node.id },
+    })
+  })
+  //   // page.matchPath is a special key that's used for matching pages
+  //   // only on the client.
+  //   if (page.path.match(/^\/projects/)) {
+  //     page.matchPath = "/projects/*"
+
+  //     // Update the page.
+  //     createPage(page)
+  //   }
+}
